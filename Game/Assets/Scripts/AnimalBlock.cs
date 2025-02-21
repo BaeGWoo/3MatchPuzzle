84d30b6ed09b445af2444715a5d6fee818885fac
BaeGWoo <gwbae4211@naver.com>
2025-02-21 23:06:27 +0900

    퍼즐 왼쪽아래이동 오류

---
+++ Game/Assets/Scripts/AnimalBlock.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class AnimalBlock : MonoBehaviour
+{
+   [SerializeField] float curRow;
+    [SerializeField] float curCol;
+    float speed = 200.0f;
+
+
+    public void SetPosition(float row, float col)
+    {
+        curRow = row;
+        curCol = col;
+    }
+
+
+    public void MoveTo(float rowPadding, float colPadding, float animalRowPadding, float animalColPadding)
+    {
+        StartCoroutine(BlockMove(rowPadding, colPadding, animalRowPadding, animalColPadding));
+    }
+
+    IEnumerator BlockMove(float rowPadding, float colPadding, float animalRowPadding, float animalColPadding)
+    {
+        Vector2 targetPosition = new Vector2(curCol*rowPadding+animalRowPadding, curRow*colPadding+animalColPadding);
+        while(Vector2.Distance(transform.localPosition, targetPosition) > 0.01f)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, speed * Time.deltaTime);          
+            yield return null;
+            targetPosition = new Vector2(curCol * rowPadding + animalRowPadding, curRow * colPadding + animalColPadding);
+
+        }
+
+        transform.localPosition = targetPosition;
+    }
+}
--- Game/Assets/Scripts/Board.cs
+++ Game/Assets/Scripts/Board.cs
@@ -1,22 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Threading;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
     [SerializeField] GameObject[] BackgroundBlock;
-    [SerializeField] int[,] boardMap=new int[9,7];
+    [SerializeField] Sprite[] AnimalBlock;
+    [SerializeField] GameObject AnimalPrefab;
+    [SerializeField] int[,] boardMap=new int[10,7];
+
     [SerializeField] int rowPadding;
     [SerializeField] int colPadding;
+    [SerializeField] int animalRowPadding;
+    [SerializeField] int animalColPadding;
+    [SerializeField] List<int> animalNumbers = new List<int>();
+    public int[,] boardTable = new int[10, 7];
+    public GameObject[,] dynamicBoard=new GameObject[10, 7];
     void Start()
     {
+        for(int i = 0; i < AnimalBlock.Length; i++)
+        {
+            animalNumbers.Add(i);
+        }
+
         boardMap[2, 5] = 1;
         boardMap[6, 2] = 2;
-        boardMap[0, 4] = 3;
+        boardMap[1, 4] = 3;
+
         rowPadding = 120;
         colPadding = -125;
-        for(int i = 0; i < 9; i++)
+
+        animalRowPadding = 15;
+        animalColPadding = -10;
+        
+
+        for (int i = 1; i < 10; i++)
         {
             for(int j = 0; j < 7; j++)
             {
@@ -27,11 +49,235 @@ public class Board : MonoBehaviour
                 tempBlock.transform.localPosition = new Vector2(j*rowPadding,i*colPadding);
             }
         }
+
+
+        int count = 0;
+
+        for (int i = 0; i < 7; i++)
+        {
+            int animalNum=animalNumbers[Random.Range(0, animalNumbers.Count)];
+            if (i > 0 && boardMap[0, i - 1] == animalNum)
+                count++;
+            else count = 0;
+
+            if (count >= 3)
+            {
+                int temp = animalNum;
+                animalNumbers.Remove(animalNum);
+                animalNum= animalNumbers[Random.Range(0, animalNumbers.Count)];
+                animalNumbers.Add(temp);
+                count = 0;
+            }
+
+            if (boardTable[1, i] == 0 && boardMap[1, i] <= 2)
+            {
+                CreateNewBlock(0 , i,animalNum);
+                BlockMoveCheck(0, i);
+            }
+        }
+    }
+    private void Update()
+    {
+        int count = 0;
+        for (int i = 0; i < 7; i++)
+        {
+            int animalNum = animalNumbers[Random.Range(0, animalNumbers.Count)];
+            if (i > 0 && boardMap[0, i - 1] == animalNum)
+                count++;
+            else count = 0;
+
+            if (count >= 3)
+            {
+                int temp = animalNum;
+                animalNumbers.Remove(animalNum);
+                animalNum = animalNumbers[Random.Range(0, animalNumbers.Count)];
+                animalNumbers.Add(temp);
+                count = 0;
+            }
+
+            if (boardTable[1, i] == 0 && boardMap[1,i]<=2)
+            {
+                CreateNewBlock(0, i, animalNum);
+                BlockMoveCheck(0, i);
+            }
+        }
+
+        
     }
 
-    // Update is called once per frame
-    void Update()
+   
+
+    public bool CheckTable(int row, int col)
     {
+        bool result=false;
+
+        if (boardTable[row, col] == 0)
+            result = true;
+
+
+        return result;
+    }
+
+    public void BlockMoveCheck(int row, int col)
+    {
+        BlockDownCheck(row, col);
+        BlockLeftCheck(row, col);
+       //bool isMoving = false;
+       //int x=row;
+       //int y=col;
+
+
+        //아래
+        //if (row+1<10)
+        //{
+        //    if (boardMap[row + 1, col] <= 2)
+        //    {
+        //        if (boardTable[row + 1, col] == 0)
+        //        {
+        //            isMoving = true;
+        //            x =row+1;
+        //            y = col;
+        //           
+        //            //Debug.Log("아래이동");
+        //        }
+        //    }
+        //    
+        //}
         
+
+        //왼쪽아래
+        //왼쪽에 장애물 없는경우가능
+        //else if (col - 1 > 0)
+        //{
+        //    if (boardMap[row, col-1] <= 2)
+        //    {
+        //        if (row + 1 < 10)
+        //        {
+        //            if (boardTable[row + 1, col - 1] == 0)
+        //            {
+        //                isMoving = true;
+        //                x = row + 1;
+        //                y = col-1;
+        //            }
+        //        }
+        //    }
+        //}
+        //오른쪽아래
+        //오른쪽 장애물(None,Mission,Mission2) 없는경우 가능
+        //else if (col + 1 < 7)
+        //{
+        //    if (boardMap[row, col + 1] <= 2)
+        //    {
+        //        if (row + 1 < 9)
+        //        {
+        //            if (boardTable[row+1, col+1] == 0)
+        //            {
+        //                isMoving = true;
+        //                moveToRow = row + 1;
+        //                moveToCol = col+1;
+        //                Debug.Log("오른쪽아래이동");
+        //            }
+        //        }
+        //    }
+        //}
+
+
+        //if (isMoving)
+        //{
+        //    boardTable[row, col] = 0;
+        //    boardTable[x, y] = 1;
+        //    dynamicBoard[x, y] = dynamicBoard[row, col];
+        //
+        //    MoveBlock(row, col, x, y);
+        //    BlockMoveCheck(x, y);
+        //}
+    }
+    public void BlockDownCheck(int row, int col)
+    {
+        bool isMoving = false;
+        int x = row;
+        int y = col;
+
+
+        //아래
+        if (row + 1 < 10)
+        {
+            if (boardMap[row + 1, col] <= 2)
+            {
+                if (boardTable[row + 1, col] == 0)
+                {
+                    isMoving = true;
+                    x = row + 1;
+                    y = col;
+
+                    //Debug.Log("아래이동");
+                }
+            }
+
+        }
+
+        if (isMoving)
+        {
+            boardTable[row, col] = 0;
+            boardTable[x, y] = 1;
+            dynamicBoard[x, y] = dynamicBoard[row, col];
+
+            MoveBlock(row, col, x, y);
+            BlockMoveCheck(x, y);
+        }
+    }
+
+    public void BlockLeftCheck(int row, int col)
+    {
+        bool isMoving = false;
+        int x = row;
+        int y = col;
+
+        if (col - 1 > 0)
+        {
+            if (boardMap[row, col - 1] <= 2)
+            {
+                if (row + 1 < 10)
+                {
+                    if (boardTable[row + 1, col - 1] == 0)
+                    {
+                        isMoving = true;
+                        x = row + 1;
+                        y = col - 1;
+                    }
+                }
+            }
+        }
+
+        if (isMoving)
+        {
+            boardTable[row, col] = 0;
+            boardTable[x, y] = 1;
+            dynamicBoard[x, y] = dynamicBoard[row, col];
+
+            MoveBlock(row, col, x, y);
+            BlockMoveCheck(x, y);
+        }
+    }
+
+
+
+    public void CreateNewBlock(int row, int col, int animalNum)
+    {
+        GameObject newBlock = Instantiate(AnimalPrefab);
+        newBlock.transform.SetParent(transform);
+        newBlock.transform.localPosition= new Vector2(col * rowPadding + animalRowPadding, row * colPadding + animalColPadding);
+        newBlock.transform.GetComponent<Image>().sprite = AnimalBlock[animalNum];
+        newBlock.GetComponent<AnimalBlock>().SetPosition(row,col);
+        dynamicBoard[row, col] = newBlock;
+
+    }
+
+    public void MoveBlock(int row,int col, int x,int y)
+    {
+        dynamicBoard[row, col].GetComponent<AnimalBlock>().SetPosition(x, y);
+        dynamicBoard[row,col].GetComponent<AnimalBlock>().MoveTo
+            (rowPadding , colPadding, animalRowPadding,animalColPadding);
+        dynamicBoard[row, col] = null;
     }
 }
